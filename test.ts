6b7bf179c003354172de41b44ab6b2f13e2876be
Andres Ruiz <andrsruiz@gmail.com>
2021-08-16 20:45:24 -0500

    Enums

--- test.ts
+++ test.ts
@@ -6,10 +6,19 @@ function useString(name: string): string {
 	return `Your name is ${name}`;
 }
 
-function print(output: string): void {
+function print(output: number): void {
 	console.log(`Your output is: ${output}`);
 }
 
+/* Enums */
+enum Counter {
+	One = 1,
+	Two,
+	Three
+}
+
+let num: Counter = Counter.Two;
+
 console.log(addNumber(2, 3));
 console.log(useString('Andres'));
-print('Testing');
+print(num);
