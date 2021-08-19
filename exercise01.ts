c3aab2d72b7a67c3bfd9bc756a5fac64468f1008
Andres Ruiz <andrsruiz@gmail.com>
2021-08-18 19:47:12 -0500

    Wrong output

--- exercise01.ts
+++ exercise01.ts
@@ -33,7 +33,7 @@ ukCitizen = false;
 fullName = firstName + ' ' + lastName;
 
 if(ukCitizen){
-	console.log(`My name is ${fullName}, I'm ${age}, and I'm a citizen of the United Kingdom.`);
+	console.log("My name is " + fullName + ", I'm " + age + ", and I'm a citizen of the United Kingdom.");
 }else{
-	console.log(`My name is ${fullName}, I'm ${age}, and I'm not a citizen of the United Kingdom.`);
+	console.log("My name is " + fullName + ", I'm " + age + ", and I'm not a citizen of the United Kingdom.");
 }
