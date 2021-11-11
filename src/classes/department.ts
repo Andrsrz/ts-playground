18a3a6fb6d6c90233039d3583d43e1e523dae4c0
Andres Ruiz <andrsruiz@gmail.com>
2021-11-11 09:51:08 -0600

    Private and public

--- src/classes/department.ts
+++ src/classes/department.ts
@@ -1,14 +1,26 @@
 class Department {
-	name: string;
+	private name: string;
+	private employees: string[] = [];
 
 	constructor(name: string) {
 		this.name = name;
 	}
 
-	describe() {
+	describe(this: Department) {
 		console.info(`Department: ${this.name}`);
+	}
+
+	addEmployee(this: Department, employee: string) {
+		this.employees.push(employee);
+	}
+
+	printEmployeeInfo(this: Department) {
+		console.info(`${this.employees.length} ${this.employees}`);
 	}
 }
 
 const accounting = new Department('Accounting');
 accounting.describe();
+
+accounting.addEmployee('Andres');
+accounting.printEmployeeInfo();
