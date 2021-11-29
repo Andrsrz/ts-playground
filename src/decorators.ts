7b920a98c1fe801b0b2cc6452535dfc38abf8dcc
Andres Ruiz <andrsruiz@gmail.com>
2021-11-29 12:40:24 -0600

    Decorators Factories

--- src/decorators.ts
+++ src/decorators.ts
@@ -1,12 +1,15 @@
 // Decorators often use the first letter as upper case
-function Logger(constructor: Function) {
-	console.info('Loggin...');
-	console.info(constructor);
+function Logger(logString: string) {
+	return function (constructor: Function) {
+		console.info(logString);
+		console.info(constructor);
+	}
 }
 
 // This should point to a function (not executing it)
+// If using it as a decorator factory we can pass in N arguments
 // Decorators execute when the class is defined
-@Logger
+@Logger('Loggin - Persona')
 class Persona {
 	nombre = 'Andres';
 
